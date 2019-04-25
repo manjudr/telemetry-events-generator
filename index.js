82db69e2d23ae041d92334a55b86ae17c6df175f
Manjunath Davanam <manjunathd@ilimi.in>
2019-04-25 22:54:19 +0530

    Issue #SB-11825 fix: Trace events

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ var syncEvents = () => {
         const targetEvents = Object.assign(target, events);
         var telemetryEvents = targetEvents.splice(0, BATCH_SIZE)
             // Trace Event log
-        var TRACE_LIMIT_SIZE = 100
+        var TRACE_LIMIT_SIZE = 50
         if (TOTAL_EVENTS_COUNT >= TRACE_LIMIT_SIZE) {
             telemetryEvents = telemetryEvents.concat(traceEvents)
+            console.log("telemetryEvents" + telemetryEvents)
         }
         var req = http.request(options, function(res) {
             var chunks = [];
